7d124728affc224ada0ef89c05f445fab556eac9
unimportant22 <ajemerenciano3@gmail.com>
2019-05-28 09:28:42 -0400

    test

--- src/Game.java
+++ src/Game.java
@@ -1,98 +1,98 @@
 public class Game
 {
-  private Grid grid;
-  private int userRow;
-  private int userCol;
-  private int msElapsed;
-  private int timesGet;
-  private int timesAvoid;
-  
-  public Game()
-  {
-    grid = new Grid(5, 10);
-    userRow = 0;
-    userCol = 0;
-    msElapsed = 0;
-    timesGet = 0;
-    timesAvoid = 0;
-    updateTitle();
-    grid.setImage(new Location(userRow, 0), "user.gif");
-  }
-  
-  public void play()
-  {
-    while (!isGameOver())
+    private Grid grid;
+    private int userRow;
+    private int userCol;
+    private int msElapsed;
+    private int timesGet;
+    private int timesAvoid;
+
+    public Game()
     {
-      grid.pause(100);
-      handleKeyPress();
-      if (msElapsed % 300 == 0)
-      {
-        scrollLeft();
-        populateRightEdge();
-      }
-      updateTitle();
-      msElapsed += 100;
-    }
-  }
-  
-  public void handleKeyPress() {
-    int key = grid.checkLastKeyPressed();
-
-    switch(key){
-      case 38 :
-        userRow--;
-        break;
-
-       case 40 :
-        userRow++;
-        break;
-
-      case 37 :
-        userCol--;
-        break;
-
-      case 39 :
-        userCol++;
-        break;
+        grid = new Grid(5, 10);
+        userRow = 0;
+        userCol = 0;
+        msElapsed = 0;
+        timesGet = 0;
+        timesAvoid = 0;
+        updateTitle();
+        grid.setImage(new Location(userRow, 0), "user.gif");
     }
 
-  }
-  
-  public void populateRightEdge() {
+    public void play()
+    {
+        while (!isGameOver())
+        {
+            grid.pause(100);
+            handleKeyPress();
+            if (msElapsed % 300 == 0)
+            {
+                scrollLeft();
+                populateRightEdge();
+            }
+            updateTitle();
+            msElapsed += 100;
+        }
+    }
 
-  }
-  
-  public void scrollLeft() {
+    public void handleKeyPress() {
+        int key = grid.checkLastKeyPressed();
 
-  }
-  
-  public void handleCollision(Location loc) {
+        switch(key){
+            case 38 :
+                userRow--;
+                break;
 
-  }
-  
-  public int getScore()
-  {
-    return 0;
-  }
-  
-  public void updateTitle()
-  {
-    grid.setTitle("Game:  " + getScore());
-  }
-  
-  public boolean isGameOver()
-  {
-    return false;
-  }
-  
-  public static void test()
-  {
-    Game game = new Game();
-    game.play();
-  }
-  
-  public static void main(String[] args)
-  {
-    test();
-  }
+            case 40 :
+                userRow++;
+                break;
+
+            case 37 :
+                userCol--;
+                break;
+
+            case 39 :
+                userCol++;
+                break;
+        }
+
+    }
+
+    public void populateRightEdge() {
+
+    }
+
+    public void scrollLeft() {
+
+    }
+
+    public void handleCollision(Location loc) {
+
+    }
+
+    public int getScore()
+    {
+        return 0;
+    }
+
+    public void updateTitle()
+    {
+        grid.setTitle("Game:  " + getScore());
+    }
+
+    public boolean isGameOver()
+    {
+        return false;
+    }
+
+    public static void test()
+    {
+        Game game = new Game();
+        game.play();
+    }
+
+    public static void main(String[] args)
+    {
+        test();
+    }
 }
